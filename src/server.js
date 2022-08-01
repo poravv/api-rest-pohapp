f4c1a9b7e91a342b7289be97cc50a09328b98536
Andres <andyvercha@gmail.com>
2022-08-01 17:09:49 -0400

    ajustes

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const app = express()
 const database = require('./database')
 const rutas = require('./config_rutas')
 const port = process.env.PORT||4000;
-const fs = require('fs')
-const https = require('https')
 
 const conecta= async()=>{
 try {
@@ -22,14 +20,6 @@ app.use(express.json({limit: '50mb',extended: true, parameterLimit:500000}));
 
 app.use(rutas)
 
-
-/*https.createServer({
-    //cert: fs.readFileSync('cert_pohapp.crt'),
-    pfx: fs.readFileSync(`${__dirname}\\certificadoV1.pfx`),
-    passphrase:'Andrecito7'
-},app).listen(port,()=>{
-    console.log("App https corriendo en el puerto: ",port)
-})*/
 
 app.get('/',(_req,res)=>{
 res.send("Bienvenido a la Api rest ")
